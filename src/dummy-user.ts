23e646132958643566b38440e1e7f2d105b0f69f
Zenus004 <satyajyoti05@gmail.com>
2024-08-27 10:48:06 +0530

    first commit

---
+++ src/dummy-user.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS =[
+    {
+        id: 'u1',
+        name: 'Satyajyoti Mohanty',
+        avatar: 'logo.png',
+      },
+      {
+        id: 'u2',
+        name: 'Emily Thompson',
+        avatar: 'logo.png',
+      },
+      {
+        id: 'u3',
+        name: 'Marcus Johnson',
+        avatar: 'logo.png',
+      },
+      {
+        id: 'u4',
+        name: 'David Miller',
+        avatar: 'logo.png',
+      },
+      {
+        id: 'u5',
+        name: 'Priya Patel',
+        avatar: 'logo.png',
+      },
+      {
+        id: 'u6',
+        name: 'Arjun Singh',
+        avatar: 'logo.png',
+      }
+];
