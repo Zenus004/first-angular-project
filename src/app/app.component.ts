ada7a8782f2c71e5310ec3aff2e25d194b78b876
Zenus004 <satyajyoti05@gmail.com>
2024-09-03 11:29:29 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from '../dummy-user';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   // template: 'Hello, {{title}}',
   styleUrls: ['./app.component.css']
@@ -15,7 +16,22 @@ import { DUMMY_USERS } from '../dummy-user';
 export class AppComponent {
   title = 'Satyajyoti';
   users = DUMMY_USERS;
+  // selectedUser = {name:''};
+
+
+  selectedUserId ?: string;
+  get selectedUser(){
+    return this.users.find((user)=> user.id == this.selectedUserId);
+  }
+
+
+  
   onSelectUser(id: string){
-    console.log("Selected user is" + id);
+    console.log('Selected user is '+ id);
+    this.selectedUserId = id;
+    // for(let user of this.users){
+    //   if(user.id == id)
+    //     this.selectedUser = user
+    // }
   }
 }
