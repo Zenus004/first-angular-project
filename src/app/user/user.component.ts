7a13273bfca6f6b0c33b6294e70794d6a8b2290e
Zenus004 <satyajyoti05@gmail.com>
2024-09-04 10:18:43 +0530

    update

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -15,9 +15,15 @@ export class UserComponent {
   // @Input({required: true}) id !: string;
   // @Input({required: true}) avatar !: string;
   // @Input({required: true}) name !: string;
-  id = input.required<string>();
-  name = input.required<string>();
-  avatar = input.required<string>();
+  // id = input.required<string>();
+  // name = input.required<string>();
+  // avatar = input.required<string>();
+
+  @Input({required: true}) user !: {
+    id: string
+    name: string
+    avatar: string
+  }
 
   // @Output() select = new EventEmitter<string>()
   select = output<string>()
@@ -25,12 +31,12 @@ export class UserComponent {
     console.log("The user click the button");
     // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     // this.selectedUser.set(DUMMY_USERS[randomIndex]);
-    this.select.emit(this.id());
+    this.select.emit(this.user.id);
   }
 
   // selectedUser = signal (DUMMY_USERS[random_Index]);
 
-  imagePath = computed(()=>'/users/' + this.avatar());
+  imagePath = computed(()=>'/users/' + this.user.avatar);
 
   // get imagePath(){
   //   return '/users/' + this.avatar();
