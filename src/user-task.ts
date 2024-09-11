4534d654c0890cbb59a2644c2fca2cc6e20a9de4
Zenus004 <satyajyoti05@gmail.com>
2024-09-11 11:06:32 +0530

    added task

---
+++ src/user-task.ts
@@ -0,0 +1,43 @@
+export const USER_TASK =[{
+        userid: 'u1',
+        taskid:'t1',
+        title: 'Anything',
+        duedate: '10-09-24',
+        summery: 'Nothing'
+    },
+    {
+        userid: 'u2',
+        taskid:'t2',
+        title: 'Anything',
+        duedate: '10-09-24',
+        summery: 'Nothing'
+    },
+    {
+        userid: 'u3',
+        taskid:'t3',
+        title: 'Anything',
+        duedate: '10-09-24',
+        summery: 'Nothing'
+    },
+    {
+        userid: 'u4',
+        taskid:'t4',
+        title: 'Anything',
+        duedate: '10-09-24',
+        summery: 'Nothing'
+    },
+    {
+        userid: 'u5',
+        taskid:'t5',
+        title: 'Anything',
+        duedate: '10-09-24',
+        summery: 'Nothing'
+    },
+    {
+        userid: 'u6',
+        taskid:'t6',
+        title: 'Anything',
+        duedate: '10-09-24',
+        summery: 'Nothing'
+    }
+];
