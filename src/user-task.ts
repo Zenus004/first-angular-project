53cb5304bc9eb3d967645da351f08eee697c34c4
Zenus004 <satyajyoti05@gmail.com>
2024-10-03 12:46:21 +0530

    user-task update

--- src/user-task.ts
+++ src/user-task.ts
@@ -1,42 +1,63 @@
 export const USER_TASK =[{
         userid: 'u1',
         taskid:'t1',
-        title: 'Anything',
+        title: 'Angular',
+        duedate: '08-10-24',
+        summery: 'Component'
+    },
+    {
+        userid: 'u1',
+        taskid:'t2',
+        title: 'Spring Boot',
         duedate: '10-09-24',
-        summery: 'Nothing'
+        summery: 'RestAPI'
+    },
+    {
+        userid: 'u1',
+        taskid:'t3',
+        title: 'Android Dev',
+        duedate: '08-10-24',
+        summery: 'Project Review'
+    },
+    {
+        userid: 'u2',
+        taskid:'t1',
+        title: 'Adv.Java',
+        duedate: '07-10-24',
+        summery: 'Project Review'
     },
     {
         userid: 'u2',
         taskid:'t2',
-        title: 'Anything',
-        duedate: '10-09-24',
-        summery: 'Nothing'
+        title: 'Android Dev',
+        duedate: '08-10-24',
+        summery: 'Project Review'
     },
     {
         userid: 'u3',
         taskid:'t3',
-        title: 'Anything',
+        title: 'Spring Boot',
         duedate: '10-09-24',
-        summery: 'Nothing'
+        summery: 'RestAPI'
     },
     {
         userid: 'u4',
         taskid:'t4',
-        title: 'Anything',
-        duedate: '10-09-24',
-        summery: 'Nothing'
+        title: 'Android Dev',
+        duedate: '10-10-24',
+        summery: 'Project Review'
     },
     {
         userid: 'u5',
         taskid:'t5',
-        title: 'Anything',
+        title: 'C Programming',
         duedate: '10-09-24',
         summery: 'Nothing'
     },
     {
         userid: 'u6',
         taskid:'t6',
-        title: 'Anything',
+        title: 'Python Dev',
         duedate: '10-09-24',
         summery: 'Nothing'
     }
