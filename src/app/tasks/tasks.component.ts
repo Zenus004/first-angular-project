f54fe96ab65d4bb2bbace9e54967cdb5cdb1ce6c
Zenus004 <satyajyoti05@gmail.com>
2024-10-29 11:15:05 +0530

    update new-task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,11 +2,13 @@ import { Component, Input, input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { USER_TASK } from '../../user-task';
 import { NgFor } from '@angular/common';
+import { NewTaskComponent } from "./new-task/newTask.component";
+import { NewTaskData } from './new-task/newTaskData.model';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent, NgFor],
+  imports: [TaskComponent, NgFor, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -14,9 +16,30 @@ export class TasksComponent {
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter(task => task.taskid !== id); // taskid instead of id
   }
+
+  onCancelAddTask(){
+    this.isAddingTask = false;
+  }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
+  }
+
+  onAddTask(newTask: NewTaskData){
+    this.tasks.unshift({
+      taskid: new Date().getTime().toString(),
+      userid: this.userId,
+      title: newTask.title,
+      summery: newTask.summery,
+      duedate: newTask.date
+    });
+    this.isAddingTask = false;
+  }
+
   // name = input.required<string>();
   @Input() userId!: string;
   @Input() userName!: string;
+  isAddingTask = false;
   tasks = USER_TASK;
 
   get userTasks() {
